f7026c54918f49cb48285fe67c60dc1997ad1b8e
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-29 21:34:58 -0700

    Fill out schema of tables

--- server/schema.sql
+++ server/schema.sql
@@ -3,13 +3,41 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  ID int NOT NULL,
+  message varchar(255),
+  PRIMARY KEY (ID)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  ID int NOT NULL,
+  name varchar(20),
+  PRIMARY KEY (ID)
+);
 
+CREATE TABLE rooms (
+  ID int NOT NULL,
+  name varchar(30),
+  PRIMARY KEY (ID)
+);
 
+CREATE TABLE users_messages (
+  ID int NOT NULL,
+  user_id int,
+  message_id int,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (user_id) REFERENCES users(ID),
+  FOREIGN KEY (message_id) REFERENCES messages(ID)
+);
 
+CREATE TABLE messages_rooms (
+  ID int NOT NULL,
+  message_id int,
+  room_id int,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (message_id) REFERENCES messages(ID),
+  FOREIGN KEY (room_id) REFERENCES rooms(ID)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql

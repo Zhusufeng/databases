3b175e71691f9ee0cfe81a639bdaa5690a0b6984
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-29 22:06:57 -0700

    Simplify schemas

--- server/schema.sql
+++ server/schema.sql
@@ -21,23 +21,34 @@ CREATE TABLE rooms (
   PRIMARY KEY (ID)
 );
 
-CREATE TABLE users_messages (
+CREATE TABLE chat_history (
   ID int NOT NULL,
-  user_id int,
-  message_id int,
+  user_id int NOT NULL,
+  message_id int NOT NULL,
+  room_id int NOT NULL,
   PRIMARY KEY (ID),
   FOREIGN KEY (user_id) REFERENCES users(ID),
-  FOREIGN KEY (message_id) REFERENCES messages(ID)
-);
-
-CREATE TABLE messages_rooms (
-  ID int NOT NULL,
-  message_id int,
-  room_id int,
-  PRIMARY KEY (ID),
   FOREIGN KEY (message_id) REFERENCES messages(ID),
   FOREIGN KEY (room_id) REFERENCES rooms(ID)
 );
+
+-- CREATE TABLE users_messages (
+--   ID int NOT NULL,
+--   user_id int,
+--   message_id int,
+--   PRIMARY KEY (ID),
+--   FOREIGN KEY (user_id) REFERENCES users(ID),
+--   FOREIGN KEY (message_id) REFERENCES messages(ID)
+-- );
+
+-- CREATE TABLE messages_rooms (
+--   ID int NOT NULL,
+--   message_id int,
+--   room_id int,
+--   PRIMARY KEY (ID),
+--   FOREIGN KEY (message_id) REFERENCES messages(ID),
+--   FOREIGN KEY (room_id) REFERENCES rooms(ID)
+-- );
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql

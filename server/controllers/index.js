140fcc427c633eb02f7bf667c258b2e4e3be0e6d
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-30 16:59:54 -0700

    Separate models to have DB tasks

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,33 +3,22 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) { // a function which handles a get request for all messages
-      // Use models
+      // Use models - call it for database data?
       // Read Data from database.
       // Send back all messages through messages in controllers/index.js!
       // Stringify it (?)
       console.log('You are in GET messages inside controllers/index.js');
-      response.writeHead(200);
-      response.end('You have exited the GET messages inside controllers/index.js');
+      models.get.messages(req, res); // may need promise since async code
+
     },
     post: function (req, res) { // a function which handles posting a message to the database
-      console.log('You are in POST messages inside controllers/index.js');
-      // Use models
+      // Use models - send incoming data to models to write to database
       // Read incoming data
       // Write it to the database
       // Send back what was written
       // Stringify it (?)
-      var body = '';
-
-      request.on('data', function(chunk) {
-        body += chunk;
-      });
-
-      request.on('end', function() {
-        console.log(body.toString());
-        data.results.push(body); // This should be where you write to the database!
-        response.writeHead(201);
-        response.end('You have exited the POST messages inside controllers/index.js');
-      });
+      console.log('You are in POST messages inside controllers/index.js');
+      models.post.messages(req, res); // may need promise since async code
     }
   },
 
@@ -41,6 +30,7 @@ module.exports = {
       // Send back all messages through messages in controllers/index.js!
       // Stringify it (?)
       console.log('You are in GET users inside controllers/index.js');
+      models.get.users(); // may need promise since async code
       response.writeHead(200);
       response.end('You have exited the GET users inside controllers/index.js');
     },
@@ -60,6 +50,7 @@ module.exports = {
       request.on('end', function() {
         console.log(body.toString());
         data.results.push(body); // This should be where you write to the database!
+        models.post.users(); // may need promise since async code
         response.writeHead(201);
         response.end('You have exited the POST users inside controllers/index.js');
       });
--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,50 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (req, res) { // a function which produces all the messages
+      // Contact database
+      // Read from database
+      // Return all messages
+      console.log('You are in GET messages inside models/index.js');
+      db.query('', function() {});
+      response.writeHead(200);
+      response.end('You have exited the GET messages inside models/index.js');
+    },
+    post: function (req, res) { // a function which can be used to insert a message into the database
+      // Contact database
+      // Save data to database
+      console.log('You are in POST messages inside models/index.js');
+
+      var body = '';
+
+      request.on('data', function(chunk) {
+        body += chunk;
+      });
+
+      db.query('INSERT INTO ', , function(err, result) {
+        if (err) { throw err; }
+
+        request.on('end', function() {
+          console.log(body.toString());
+          data.results.push(body); // This should be where you write to the database!
+          response.writeHead(201);
+          response.end('You have exited the POST messages inside models/index.js');
+      });
+    }
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () { // a function which produces all the users
+      // Contact database
+      // Read from database
+      // Return all users
+
+    },
+    post: function () { // a function which can be used to insert a user into the database
+      // Contact database
+      // Save data to database
+    }
   }
 };
 

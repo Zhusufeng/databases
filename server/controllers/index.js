af18618f412053d6e1b6d933dd4d3d4af7dcf884
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-30 19:53:56 -0700

    Fill out GET and POST in controllers/index.js

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,22 +3,26 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) { // a function which handles a get request for all messages
-      // Use models - call it for database data?
-      // Read Data from database.
-      // Send back all messages through messages in controllers/index.js!
-      // Stringify it (?)
+      // Use models - call it for database data
       console.log('You are in GET messages inside controllers/index.js');
-      models.get.messages(req, res); // may need promise since async code
+
+      models.messages.get(function(err, results) {
+        if (err) { throw err; }
+        res.json(results); // send back as JSON data thru Express
+      }); // may need promise since async code
 
     },
+
     post: function (req, res) { // a function which handles posting a message to the database
       // Use models - send incoming data to models to write to database
-      // Read incoming data
-      // Write it to the database
-      // Send back what was written
-      // Stringify it (?)
       console.log('You are in POST messages inside controllers/index.js');
-      models.post.messages(req, res); // may need promise since async code
+
+      // MySQL takes in array
+      var params = [ req.body[text], req.body[username], req.body[roomname] ];
+      models.messages.post(params, function(err, results) {
+        if (err) { throw err; }
+        res.json(results);
+      }); // may need promise since async code
     }
   },
 
@@ -26,34 +30,24 @@ module.exports = {
     // Ditto as above
     get: function (req, res) { // a function which handles a get request for all users
       // Use models
-      // Read Data from database.
-      // Send back all messages through messages in controllers/index.js!
-      // Stringify it (?)
       console.log('You are in GET users inside controllers/index.js');
-      models.get.users(); // may need promise since async code
-      response.writeHead(200);
-      response.end('You have exited the GET users inside controllers/index.js');
+
+      models.users.get(function(err, results) {
+        if (err) { throw err; }
+        res.json(results); // send back as JSON data thru Express
+      }); // may need promise since async code
     },
+
     post: function (req, res) { // a function which handles posting a user to the database
       console.log('You are in POST users inside controllers/index.js');
       // Use models
-      // Read incoming data
-      // Write it to the database
-      // Send back what was written
-      // Stringify it (?)
-      var body = '';
 
-      request.on('data', function(chunk) {
-        body += chunk;
-      });
-
-      request.on('end', function() {
-        console.log(body.toString());
-        data.results.push(body); // This should be where you write to the database!
-        models.post.users(); // may need promise since async code
-        response.writeHead(201);
-        response.end('You have exited the POST users inside controllers/index.js');
-      });
+      // MySQL takes in array
+      var params = [ req.body[username] ];
+      models.users.post(params, function(err, results) {
+        if (err) { throw err; }
+        res.json(results);
+      }); // may need promise since async code
     }
   }
 };

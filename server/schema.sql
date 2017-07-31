5fe1ae4115d3ecf1f02530f4a3864d2e1537a1c5
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-30 19:08:34 -0700

    Simplify schema for now

--- server/schema.sql
+++ server/schema.sql
@@ -3,34 +3,37 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  ID int NOT NULL,
-  message varchar(255),
-  PRIMARY KEY (ID)
+  ID int NOT NULL AUTO_INCREMENT,
+  userid int NOT NULL,
+  message varchar(255) NOT NULL,
+  roomname varchar(30) NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (userid) REFERENCES users(ID)
 );
 
 /* Create other tables and define schemas for them here! */
 CREATE TABLE users (
-  ID int NOT NULL,
-  name varchar(20),
+  ID int NOT NULL AUTO_INCREMENT,
+  username varchar(20) NOT NULL,
   PRIMARY KEY (ID)
 );
 
-CREATE TABLE rooms (
-  ID int NOT NULL,
-  name varchar(30),
-  PRIMARY KEY (ID)
-);
+-- CREATE TABLE rooms (
+--   ID int NOT NULL,
+--   name varchar(30),
+--   PRIMARY KEY (ID)
+-- );
 
-CREATE TABLE chat_history (
-  ID int NOT NULL,
-  user_id int NOT NULL,
-  message_id int NOT NULL,
-  room_id int NOT NULL,
-  PRIMARY KEY (ID),
-  FOREIGN KEY (user_id) REFERENCES users(ID),
-  FOREIGN KEY (message_id) REFERENCES messages(ID),
-  FOREIGN KEY (room_id) REFERENCES rooms(ID)
-);
+-- CREATE TABLE chat_history (
+--   ID int NOT NULL,
+--   user_id int NOT NULL,
+--   message_id int NOT NULL,
+--   room_id int NOT NULL,
+--   PRIMARY KEY (ID),
+--   FOREIGN KEY (user_id) REFERENCES users(ID),
+--   FOREIGN KEY (message_id) REFERENCES messages(ID),
+--   FOREIGN KEY (room_id) REFERENCES rooms(ID)
+-- );
 
 -- CREATE TABLE users_messages (
 --   ID int NOT NULL,

9e01030709ae15a9a8288881decb344c002344ed
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-30 20:57:48 -0700

    Update name of db connection to db

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,12 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
-exports.dbConnection = mysql.createConnection({
+exports.db = mysql.createConnection({
   user: 'root',
   password: 'root', // put in the password
   database: 'chat'
 });
-exports.dbConnection.connect(function(err) {
+exports.db.connect(function(err) {
   if (err) { throw err; }
   console.log('We are connected to the Database chat!');
 });

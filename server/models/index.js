ddb0ae5b5a1e49965d7eea1dea4f7e3d03c4ae34
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-30 19:33:53 -0700

    Fill out queries in models/index.js

--- server/models/index.js
+++ server/models/index.js
@@ -2,46 +2,51 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function (req, res) { // a function which produces all the messages
+    get: function (callback) { // a function which produces all the messages
       // Contact database
       // Read from database
       // Return all messages
       console.log('You are in GET messages inside models/index.js');
-      db.query('', function() {});
-      response.writeHead(200);
-      response.end('You have exited the GET messages inside models/index.js');
+
+      var queryString = 'SELECT messages.id, messages.message, messages.roomname, users.username \
+                         FROM messages LEFT OUTER JOIN users on (messages.userid = users.id) \
+                         ORDER BY messages.id DESC';
+      db.query(queryString, function(err, results) {
+        callback(results);
+      });
+
     },
-    post: function (req, res) { // a function which can be used to insert a message into the database
+
+    post: function (params, callback) { // a function which can be used to insert a message into the database
       // Contact database
       // Save data to database
       console.log('You are in POST messages inside models/index.js');
 
-      var body = '';
-
-      request.on('data', function(chunk) {
-        body += chunk;
-      });
-
-      db.query('INSERT INTO ', , function(err, result) {
+      var queryString = 'INSERT INTO messages(message, userid, roomname) \
+                         values (' + params.message + ', (SELECT id FROM users WHERE username = ' + params.username + 'limit 1), '+ params.roomname + ')';
+      db.query(queryString, function(err, results) {
         if (err) { throw err; }
-
-        request.on('end', function() {
-          console.log(body.toString());
-          data.results.push(body); // This should be where you write to the database!
-          response.writeHead(201);
-          response.end('You have exited the POST messages inside models/index.js');
+        callback(results);
       });
+
     }
   },
 
   users: {
     // Ditto as above.
-    get: function () { // a function which produces all the users
+    get: function (callback) { // a function which produces all the users
       // Contact database
       // Read from database
       // Return all users
+      console.log('You are in GET users inside models/index.js');
 
+      var queryString = 'SELECT * FROM users';
+      db.query(queryString, function(err, results) {
+        if (err) { throw err; }
+        callback(results);
+      });
     },
+
     post: function () { // a function which can be used to insert a user into the database
       // Contact database
       // Save data to database
